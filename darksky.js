4a2f5b26947581d70fadc0f2a1f24eff4b8a84a6
SteveODell <saodell@mac.com>
2017-03-21 15:40:17 -0600

    basic weather API functionality

---
+++ darksky.js
@@ -0,0 +1,29 @@
+$(document).ready(function($) {
+// This will not run!! Needs API key!!
+const myKey = 'XXXXXXXXXXXXX'
+var getInfo = function(){
+    //Grabs the longitude and latitude
+    var myLongitude = $('#long').val();
+    var myLatitude = $('#lat').val();
+    //Makes the request
+    $.ajax({
+      url : "https://api.darksky.net/forecast/" + myKey + "/" + myLongitude + "," + myLatitude +      "?exclude=minutely,hourly,alerts,flags",
+      dataType : "jsonp",
+      success : function(pJSON) {
+        var whereAmI = pJSON['timezone'];
+        var myTemp = pJSON['currently']['temperature'];
+        var myWeather = ("Current temperature in " + whereAmI + " is: " + myTemp);
+        $('.result').html("<h3>" + myWeather + "</h3>")
+        $('.lottsoweather').html(
+          "<li>Max Temp: " + pJSON['daily']['data'][0]['temperatureMax'] + "</li>" +
+          "<li>Min Temp: " + pJSON['daily']['data'][0]['temperatureMin'] + "</li>" +
+          "<li>Max Wind: " + pJSON['daily']['data'][0]['windSpeed']  + "</li>" +
+          "<li>Precip: " + pJSON['daily']['data'][0]['precipProbability']  + "% </li>" +
+          "<li> Summary: " + pJSON['daily']['summary']  + "</li>"
+        )
+      }
+    });
+}
+
+$('#search').click(getInfo);
+});
