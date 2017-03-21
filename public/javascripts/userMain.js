666d3a22bce71f5d96cede903bcf400a1be01a21
lanefaison2 <faisonusmc@gmail.com>
2017-03-21 13:46:23 -0600

    jquery

--- public/javascripts/userMain.js
+++ public/javascripts/userMain.js
@@ -4,7 +4,25 @@ var server = heroku
 $(document).ready(() => {
   console.log('linked');
   $.get(`${server}/locations`,(data) =>{
-      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        $('.scrollmenu').append(`<div class="border userLocation">
+                <h3 class="location">${data[i].name}</h3>
+                <!-- potential gif load based on current weather ie sun, rain, clouds, windy- think yahoo weather! -->
+                <img src="http://placehold.it/150x150">
+                <h4 class="temp">temperature</h4>
+                <h4 class="precip">precipitation</h4>
+                <h4 class="wind">wind</h4>
+
+                <div class="btn-group btn-group-justified btn-width" role="group" aria-label="...">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default edit-btn">Edit</button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default delete-btn">Delete</button>
+                  </div>
+                </div>
+              </div>`)
+      }
     })
 
   //   $.ajax({
