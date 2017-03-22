3304dc02d968887c9a0630e97c9540a5b4282896
SteveODell <saodell@mac.com>
2017-03-22 09:30:14 -0600

    switched lat/long to fix bug; added weather icon to API call

--- darksky.js
+++ darksky.js
@@ -1,25 +1,26 @@
 $(document).ready(function($) {
 // This will not run!! Needs API key!!
-const myKey = '#############################'
-var getInfo = function(){
+const myKey = '#########################'
+let getInfo = function(){
     //Grabs the longitude and latitude
-    var myLongitude = $('#long').val();
-    var myLatitude = $('#lat').val();
+    let myLongitude = $('#long').val();
+    let myLatitude = $('#lat').val();
     //Makes the request
     $.ajax({
-      url : "https://api.darksky.net/forecast/" + myKey + "/" + myLongitude + "," + myLatitude +      "?exclude=minutely,hourly,alerts,flags",
+      url : "https://api.darksky.net/forecast/" + myKey + "/" + myLatitude + "," + myLongitude + "?exclude=minutely,hourly,alerts,flags",
       dataType : "jsonp",
-      success : function(pJSON) {
-        var whereAmI = pJSON['timezone'];
-        var myTemp = pJSON['currently']['temperature'];
-        var myWeather = ("Current temperature in " + whereAmI + " is: " + myTemp);
+      success : function(darkData) {
+        let whereAmI = darkData['timezone'];
+        let myTemp = darkData['currently']['temperature'];
+        let myWeather = ("Current temperature in " + whereAmI + " is: " + myTemp);
         $('.result').html("<h3>" + myWeather + "</h3>")
         $('.lottsoweather').html(
-          "<li>Max Temp: " + pJSON['daily']['data'][0]['temperatureMax'] + "</li>" +
-          "<li>Min Temp: " + pJSON['daily']['data'][0]['temperatureMin'] + "</li>" +
-          "<li>Max Wind: " + pJSON['daily']['data'][0]['windSpeed']  + "</li>" +
-          "<li>Precip: " + pJSON['daily']['data'][0]['precipProbability']  + "% </li>" +
-          "<li> Summary: " + pJSON['daily']['summary']  + "</li>"
+          "<li>Max Temp: " + darkData['daily']['data'][0]['temperatureMax'] + "</li>" +
+          "<li>Min Temp: " + darkData['daily']['data'][0]['temperatureMin'] + "</li>" +
+          "<li>Max Wind: " + darkData['daily']['data'][0]['windSpeed']  + "</li>" +
+          "<li>Precip: " + darkData['daily']['data'][0]['precipProbability']  + "% </li>" +
+          "<li> Summary: " + darkData['daily']['summary']  + "</li>"
+          + "<li>" + darkData['currently']['icon'] + "</li>"
         )
       }
     });
