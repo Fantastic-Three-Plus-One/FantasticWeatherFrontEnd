cbfaa81cd8f4cf1b213189b4380edd60a43b13bc
SteveODell <saodell@mac.com>
2017-03-21 21:39:40 -0600

    added multiple marker functionality

--- mapsscripts.js
+++ mapsscripts.js
@@ -55,5 +55,40 @@ function initMap() {
     infowindowContent.children['place-name'].textContent = place.name;
     infowindowContent.children['place-address'].textContent = address;
     infowindow.open(map, marker);
+
   });
 }
+
+
+// marking multiple locatons:
+
+//    Add multiple markers to map
+//    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+//    Place each marker on the map
+//    for( i = 0; i < markers.length; i++ ) {
+//     var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+//     bounds.extend(position);
+//     marker = new google.maps.Marker({
+//         position: position,
+//         map: map,
+//         title: markers[i][0]
+//     });
+
+//    Add info window to marker
+//    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+//      return function() {
+//         infoWindow.setContent(infoWindowContent[i][0]);
+//         infoWindow.open(map, marker);
+//      }
+//    })(marker, i));
+
+//    Center the map to fit all markers on the screen
+//    map.fitBounds(bounds);
+//    }
+
+//    Set zoom level
+//    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+//      this.setZoom(14);
+//      google.maps.event.removeListener(boundsListener);
+//    });
