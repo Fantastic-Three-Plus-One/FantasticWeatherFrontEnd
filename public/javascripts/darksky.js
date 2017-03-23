e08681fd810d5adbe1b5bd25931c0a35b7f6daa7
SteveODell <saodell@mac.com>
2017-03-22 22:55:38 -0600

    fixed weather forecaster, future date function, append html

--- public/javascripts/darksky.js
+++ public/javascripts/darksky.js
@@ -1,30 +1,34 @@
 $(document).ready(function($) {
-  // This will not run!! Needs API key!!
-  const myKey = '2408a490c435cf59e2d5fb99dddce6d4'
-  var getInfo = function(){
-      //Grabs the longitude and latitude
-      var myLongitude = $('#long').val();
-      var myLatitude = $('#lat').val();
-      //Makes the request
-      $.ajax({
-        url : "https://api.darksky.net/forecast/" + myKey + "/" + myLongitude + "," + myLatitude +      "?exclude=minutely,hourly,alerts,flags",
-        dataType : "jsonp",
-        success : function(pJSON) {
-          console.log(pJSON);
-          var whereAmI = pJSON['timezone'];
-          var myTemp = pJSON['currently']['temperature'];
-          var myWeather = ("Current temperature in " + whereAmI + " is: " + myTemp);
-          $('.result').html("<h3>" + myWeather + "</h3>")
-          $('.lottsoweather').html(
-            "<li>Max Temp: " + pJSON['daily']['data'][0]['temperatureMax'] + "</li>" +
-            "<li>Min Temp: " + pJSON['daily']['data'][0]['temperatureMin'] + "</li>" +
-            "<li>Max Wind: " + pJSON['daily']['data'][0]['windSpeed']  + "</li>" +
-            "<li>Precip: " + pJSON['daily']['data'][0]['precipProbability']  + "% </li>" +
-            "<li> Summary: " + pJSON['daily']['summary']  + "</li>"
-          )
-        }
-      });
-  }
+// This will not run!! Needs API key!!
+const myKey = 'fd59c08b71d9a82c1248b5012aca9c44'
+let getInfo = function(){
+    //Grabs the longitude and latitude
+    let myLongitude = $('#long').val();
+    let myLatitude = $('#lat').val();
+    var myDate = $('#datepicker').val();
+    const myTime = 'T12:00:00'
+    //Makes the request
+    $.ajax({
+      url : "https://api.darksky.net/forecast/" + myKey + "/" + myLatitude + "," + myLongitude
+      + "," + myDate + myTime
+      + "?exclude=minutely,hourly,alerts,flags",
+      dataType : "jsonp",
+      success : function(darkData) {
+        let whereAmI = darkData['timezone'];
+        let myTemp = darkData['currently']['temperature'];
+        let myWeather = ("Temperature on " + myDate + " in " + whereAmI + " is: " + myTemp);
+        $('.result').html("<h3>" + myWeather + "</h3>")
+        $('.lottsoweather').html(
+          "<li>Max Temp: " + darkData['daily']['data'][0]['temperatureMax'] + "</li>" +
+          "<li>Min Temp: " + darkData['daily']['data'][0]['temperatureMin'] + "</li>" +
+          "<li>Max Wind: " + darkData['daily']['data'][0]['windSpeed']  + "</li>" +
+          "<li>Precip: " + darkData['daily']['data'][0]['precipProbability']  + "% </li>" +
+          "<li> Summary: " + darkData['daily']['summary']  + "</li>"
+          + "<li>" + darkData['currently']['icon'] + "</li>"
+        )
+      }
+    });
+}
 
 $('#search').click(getInfo);
 });
